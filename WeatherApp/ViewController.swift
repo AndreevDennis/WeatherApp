67e5b0ea6c5e528380030158855c8276a26fc75d
DennisAndreev <denisandreev1611@gmail.com>
2019-07-10 16:42:18 +0300

    ниче не пашет(пока что)

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -9,12 +9,59 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        searchBar.delegate = self
     }
-
-
+    
 }
 
+
+
+extension ViewController : UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar){
+        
+        searchBar.resignFirstResponder()
+        
+        let urlString = "https://api.apixu.com/v1/current.json?key=df6d7f5296d74441bf095518191007 &q=Kiev"
+        
+        let url = URL(string: urlString)
+        
+        var locationName:String?
+        var temperature:Double?
+        
+        let task = URLSession.shared.dataTask(with: url!) { [weak self] (data, response, error) in
+            do {
+                let json  = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as! [String : AnyObject]
+                
+                if let location = json["location"]  {
+                    locationName = location["name"] as? String
+                }
+                
+                
+                if let current = json["current"] {
+                    temperature = current["temp_c"] as? Double
+                }
+                
+                DispatchQueue.main.async {
+                    self?.cityLabel.text = locationName
+                    self?.temperatureLabel.text = "\(temperature)"
+                }
+                
+            }
+            catch let jsonError {
+                print(jsonError)
+            }
+            
+        }
+        task.resume()
+        
+    }
+}
